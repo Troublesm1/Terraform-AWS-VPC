82b04ee076fe7820d1e61af18dea9602ac44bf00
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-06-26 22:28:42 -0400

    subnet associated w/ route table

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -60,14 +60,14 @@ resource "aws_route_table" "public_route_table" {
 
 # associate public subnet az1 to "public route table"
 resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
-  subnet_id           = 
-  route_table_id      = 
+  subnet_id           = aws_subnet.public_subnet_az1.id
+  route_table_id      = aws_route_table.public_route_table.id
 }
 
 # associate public subnet az2 to "public route table"
 resource "aws_route_table_association" "public_subnet_az2_route_table_association" {
-  subnet_id           = 
-  route_table_id      = 
+  subnet_id           = aws_subnet.public_subnet_az2.id
+  route_table_id      = aws_route_table.public_route_table.id
 }
 
 # create private app subnet az1

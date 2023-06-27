02ddfee493f1f6206faf45032f62b0786bbd179a
Jovahnn Tucker <Jovahnn@yahoo.com>
2023-06-26 22:25:44 -0400

    subnet and route table created

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -11,10 +11,9 @@ resource "aws_vpc" "vpc" {
 
 # create internet gateway and attach it to vpc
 resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id    = 
-
+  vpc_id    = aws_vpc.vpc.id
   tags      = {
-    Name    = "${}-igw"
+    Name    = "${var.project_name}-igw"
   }
 }
 
@@ -23,39 +22,39 @@ data "aws_availability_zones" "available_zones" {}
 
 # create public subnet az1
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_az1_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
+  map_public_ip_on_launch = true
 
   tags      = {
-    Name    = 
+    Name    = "public subnet az1"
   }
 }
 
 # create public subnet az2
 resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = 
-  cidr_block              = 
-  availability_zone       = 
-  map_public_ip_on_launch = 
+  vpc_id                  = aws_vpc.vpc.id
+  cidr_block              = var.public_subnet_az2_cidr
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
+  map_public_ip_on_launch = true
 
   tags      = {
-    Name    = 
+    Name    = "public subnet az2"
   }
 }
 
 # create route table and add public route
 resource "aws_route_table" "public_route_table" {
-  vpc_id       = 
+  vpc_id       = aws_vpc.vpc.id
 
   route {
-    cidr_block = 
-    gateway_id = 
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
   }
 
   tags       = {
-    Name     = 
+    Name     = "public route table"
   }
 }
 
